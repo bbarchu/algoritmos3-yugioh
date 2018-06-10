8ae71166d1de8df9b4cf56f0eb68cf496be78be9
Lucas <Lucas@Sony-Vaio-Lucas>
2018-06-10 20:30:53 -0300

    Clases de pueba borradas. Plantilla limpia para empezar el Tp

--- TP-2-Al-Go-Oh!/tests/UnitTests/AlGoOhTests.java
+++ TP-2-Al-Go-Oh!/tests/UnitTests/AlGoOhTests.java
@@ -8,18 +8,5 @@ import org.junit.Test;
 import Application.AlGoOh;
 
 public class AlGoOhTests {
-	
-	@Test
-	public void obtenerValor() {
-		
-		AlGoOh algooh = new AlGoOh();
-		
-		assertEquals(3, algooh.getValor() );
-	}
-	@Test
-	public void obtenerOtroValor() {
-		
-		AlGoOh algooh = new AlGoOh();
-		assertNotEquals(40, algooh.getValor()) ;
-	}
+
 }
