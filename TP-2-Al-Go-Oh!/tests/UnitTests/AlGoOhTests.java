cd64fb48a01e463168bb10c601f6aab5b0297d1a
Lucas <Lucas@Sony-Vaio-Lucas>
2018-06-10 19:30:34 -0300

    prueba 5

--- TP-2-Al-Go-Oh!/tests/UnitTests/AlGoOhTests.java
+++ TP-2-Al-Go-Oh!/tests/UnitTests/AlGoOhTests.java
@@ -20,6 +20,6 @@ public class AlGoOhTests {
 	public void obtenerOtroValor() {
 		
 		AlGoOh algooh = new AlGoOh();
-		assertNotEquals(30, algooh.getValor());
+		assertNotEquals(40, algooh.getValor());
 	}
 }
