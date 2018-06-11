52e672e2a411150bd3741540a4a9fdb660ba4db1
Franco <Franco@Francogmz_>
2018-06-11 08:16:37 -0300

    prueba de commit. Solo comente los atributos que puso lucas para que no aparezca error

--- TP-2-Al-Go-Oh!/src/Application/AlGoOh.java
+++ TP-2-Al-Go-Oh!/src/Application/AlGoOh.java
@@ -4,12 +4,12 @@ public class AlGoOh {
 	
 	// Lucas: la forma de guardar el campo la estuvimos discutiendo con Franco pero no decidimos nada
 	
-	Campo campoJugador1;
-	Campo campoJugador2;
+//	Campo campoJugador1;
+//	Campo campoJugador2;
 	
 	
 	public AlGoOh() {
-		campoJugador1 = new Campo();
-		campoJugador2 = new Campo();
+//		campoJugador1 = new Campo();
+//		campoJugador2 = new Campo();
 	}
 }
--- TP-2-Al-Go-Oh!/src/Application/Campo.java
+++ TP-2-Al-Go-Oh!/src/Application/Campo.java
@@ -4,13 +4,13 @@ public class Campo {
 	
 	//Lucas: los puse como atributo solo por poner algo 
 	
-	ZonaCartasMonstruos cartasMonstruo;
-	ZonaCartasMagicas cartasMagicas;
-	ZonaCartasCampo cartasCampo;
-	ZonaCartasTrampa cartasTrampa;
-	Cementerio cementerio;
-	Mazo mazo;
-	Mano mano;
+//	ZonaCartasMonstruos cartasMonstruo;
+//	ZonaCartasMagicas cartasMagicas;
+//	ZonaCartasCampo cartasCampo;
+//	ZonaCartasTrampa cartasTrampa;
+//	Cementerio cementerio;
+//	Mazo mazo;
+//	Mano mano;
 	
 
 }
