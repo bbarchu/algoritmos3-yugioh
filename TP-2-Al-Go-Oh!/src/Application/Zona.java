d8bc98f81b21db35521bb632c6fba8c74e7e8715
nachonitz <nachonitz@gmail.com>
2018-06-11 21:32:54 -0300

    CampoDeBatalla, Zona, ZonaMagica, Cementerio. Empezados, no terminados

--- TP-2-Al-Go-Oh!/src/Application/Zona.java
+++ TP-2-Al-Go-Oh!/src/Application/Zona.java
@@ -1,5 +1,11 @@
 package Application;
 
+import java.util.ArrayList;
+
 public abstract class Zona {
+	private ArrayList<Carta> cartas;
 	
+	public Zona() {
+		cartas = new ArrayList<Carta>();
+	}
 }
