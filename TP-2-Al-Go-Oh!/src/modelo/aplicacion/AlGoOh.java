6c44db8b1a683dbcfc8ead06c9bb8677f18c8fac
bbarc <bbarc@LAPTOP-O526RLJS>
2018-06-21 19:31:19 -0300

    Si queda algo de turno, borrenlo. salu2

--- TP-2-Al-Go-Oh!/src/modelo/aplicacion/AlGoOh.java
+++ TP-2-Al-Go-Oh!/src/modelo/aplicacion/AlGoOh.java
@@ -15,52 +15,10 @@ public class AlGoOh {
 		jugadorNumeroUno = new Jugador();
 		jugadorNumeroDos = new Jugador();
 		
-		System.out.println ("Por favor introduzca el nombre del jugador 1:");
-		String entradaTeclado = "";
-        Scanner entradaEscaner = new Scanner (System.in); 
-        entradaTeclado = entradaEscaner.nextLine ();
-        jugadorNumeroUno.agregarNombre(entradaTeclado);
-        
-        System.out.println ("Por favor introduzca el nombre del jugador 2:");
-        entradaTeclado = entradaEscaner.nextLine ();
-		
-        jugadorNumeroDos.agregarNombre(entradaTeclado);
-		
-		
-		
-		//Lucas: Falta decidir como agregar todas las cartas al mazo de cada jugador (el orden tiene que ser aleatorio)
-		//Opciones: -dar a elegir las cartas (más dificil)
-		//			-colocar cartas random en cada uno
-		//			-colocar las mismas cartas en ambos (diferente orden)
-		
-		
-		//presentacion
-		jugadorNumeroUno.presentarJugadorRival(jugadorNumeroDos);
-		jugadorNumeroDos.presentarJugadorRival(jugadorNumeroUno);
-		
-		//Comienzo de partida
-		//Lucas: Esta parte me parece que la podemos hacer en el constructor del jugador
-		jugadorNumeroUno.tomarCincoCartasDelMazo();
-		jugadorNumeroDos.tomarCincoCartasDelMazo();
-		
-
-		int numero = (int) (Math.random() * 2) +1 ;
-		
-		if (numero == 1) {
-			//Comienza el jugador numero 1 
-			
-			Turno turno = new Turno(jugadorNumeroUno);
-			turno.faseInicial();
-		}
-		else {
-			
-			//Comienza el jugador numero 2
-			Turno turno = new Turno(jugadorNumeroDos);
-			turno.faseInicial();
 		}
 		
 		
-	}
+	
 	
 
 }
