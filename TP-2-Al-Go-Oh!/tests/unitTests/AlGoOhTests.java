6a7ea285f1cfcb512d8ebdc45eb4b819d8c2699e
Franco <Franco@Francogmz_>
2018-06-28 12:50:38 -0300

    aaa

--- TP-2-Al-Go-Oh!/tests/unitTests/AlGoOhTests.java
+++ TP-2-Al-Go-Oh!/tests/unitTests/AlGoOhTests.java
@@ -62,6 +62,33 @@ public class AlGoOhTests {
 	}
 	
 	@Test
+	public void testObtenerNombresDeCartasMagicaInvocada() {
+		
+		System.out.println("testObtenerNombresDeCartasMagicaYTrampaCorrectamenteAlEstarBocaAbajoYBocaArriba");
+		
+		AlGoOh juego = new AlGoOh();
+		
+		LinkedList<String> nombresCartasEnLaMano = juego.obtenerArregloDe10CartasEnLaManoDelJugadorActual();
+		
+		System.out.println("\nCartas en la mano que se invocaran\n");
+		for(int i = 0 ; i < nombresCartasEnLaMano.size() ; i++) System.out.println(nombresCartasEnLaMano.get(i));
+		
+		juego.colocarEnElCampoDelJugadorActual("AgujeroOscuro");
+		
+		
+		
+		LinkedList<String> nombresDeCartasMagiaInvocadas = juego.obtenerArregloDe5CartasMagiaTrampaJugadorActual();
+		
+		System.out.println("\n\tCartas Magicas Invocadas (deberian decir dorso)\n");
+		for(int i = 0 ; i < nombresDeCartasMagiaInvocadas.size() ; i++) System.out.println(nombresDeCartasMagiaInvocadas.get(i));
+		
+		
+		
+		
+	
+	}
+	
+	@Test
 	public void testObtenerNombresDeCartasMagicaYTrampaCorrectamenteAlEstarBocaAbajoYBocaArriba() {
 		
 		System.out.println("testObtenerNombresDeCartasMagicaYTrampaCorrectamenteAlEstarBocaAbajoYBocaArriba");
@@ -73,29 +100,10 @@ public class AlGoOhTests {
 		System.out.println("\nCartas en la mano que se invocaran\n");
 		for(int i = 0 ; i < nombresCartasEnLaMano.size() ; i++) System.out.println(nombresCartasEnLaMano.get(i));
 		
-		for(int i = 0 ; i < nombresCartasEnLaMano.size(); i++) {
-			
-			juego.colocarEnElCampoDelJugadorActual(nombresCartasEnLaMano.get(i));
-		}
+		juego.colocarEnElCampoDelJugadorActual("AgujeroOscuro");
 		
-		//Deberia obtener la palabra Dorso por cada una de las que se invoco (empiezan boca abajo)
-		
-		LinkedList<String> nombresDeCartasMagiaInvocadas = juego.obtenerArregloDe5CartasMagiaTrampaJugadorActual();
-		System.out.println("\n\tCartas Magicas Invocadas (deberian decir dorso)\n");
-		for(int i = 0 ; i < nombresDeCartasMagiaInvocadas.size() ; i++) System.out.println(nombresDeCartasMagiaInvocadas.get(i));
-	
-		//volteo las cartas (AgujeroOscuro es la unica magica que tendra
-		
-		juego.activarCartaMagicaDelJugadorActual("AgujeroOscuro");
-		
-		//Ahora si deberia devolver los nombres de las cartas magicas, porque fueron volteadas
-		nombresDeCartasMagiaInvocadas = juego.obtenerArregloDe5CartasMagiaTrampaJugadorActual();
-		
-		System.out.println("\n\tCartas Magicas Invocadas ( las activadas deberian decir sus nombres y las que dicen Dorso deben ser Trampas, si dice vacio es por no habia la cartas suficiente y se relleno)\n");
-		for(int i = 0 ; i < nombresDeCartasMagiaInvocadas.size() ; i++) System.out.println(nombresDeCartasMagiaInvocadas.get(i));
-		
-		
-	
+		LinkedList<String> nombresCartasMagicas =juego.obtenerArregloDe5CartasMagiaTrampaJugadorActual();
+		for(int i = 0 ; i < nombresCartasMagicas.size() ; i++) System.out.println(nombresCartasMagicas.get(i));
 	}
 	
 	@Test
